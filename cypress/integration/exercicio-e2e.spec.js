c141ab2ab953195c22a46f50b44627cea570efa0
vitoria <finkedificacoes@gmail.com>
2023-10-17 10:04:09 +0100

    adicionando produtos e acedendo checkout

--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -10,11 +10,44 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('produtos/page/17')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        cy.get('.post-2858 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Black').click()
+        cy.get('.input-text').clear().type(2)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.visit('produtos/page/17')
+
+        cy.get('.post-2913 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-33').click()
+        cy.get('.button-variable-item-Gray').click()
+        cy.get('.input-text').clear().type(3)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.visit('produtos/page/17')
+
+        cy.get('.post-3641 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Black').click()
+        cy.get('.input-text').clear().type(3)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.visit('produtos/page/17')
+
+        cy.get('.post-3019 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.button-variable-item-34').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(1)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        
+
     });
 
 
